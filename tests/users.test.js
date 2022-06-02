5ca3ab8dad234af37be07820badb09b47ce03481
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-06-02 01:05:59 -0500

    Add tests for creating users

---
+++ tests/users.test.js
@@ -0,0 +1,49 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import app from '../app.js';
+
+const USERS_PATH = '/users';
+
+const NEW_USER = {
+  username: 'test',
+  password: 'password',
+  email: 'test@example.com',
+  birthdate: '2000-01-01',
+  bio: 'I a a test user'
+};
+
+describe('Users routes', () => {
+  beforeAll(async () => {
+    await mongoose.connect(global.__MONGO_URI__);
+  });
+
+  afterAll(() => {
+    mongoose.disconnect();
+  });
+
+  describe('POST /users', () => {
+    it('Should create user', async () => {
+      const response = await request(app).post(USERS_PATH).send(NEW_USER);
+      expect(response.statusCode).toBe(201);
+      expect(response.body.token).toBeDefined();
+    });
+
+    it('Should return bad request when missing fields', async () => {
+      const response = await request(app).post(USERS_PATH).send({});
+      expect(response.statusCode).toBe(400);
+      expect(response.body.error).toBe('Missing fields');
+    });
+
+    it('Should return bad request when user already exists', async () => {
+      const response = await request(app).post(USERS_PATH).send(NEW_USER);
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe('User already exists');
+    });
+  });
+
+  // it('Should return all users', async () => {
+  //   const response = await request(app).get(USERS_PATH);
+  //   expect(response.statusCode).toBe(200);
+  //   expect(response.body.length).toBe(0);
+  // });
+});
